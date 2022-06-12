5eab1ee57cc6952d79968b4538c88baa46e0e4d5
melgers <hans.melgers@gmail.com>
2022-06-12 21:35:12 +0200

    Ready to go

--- chapter-1-building-blocks/src/main/java/HelloWorld.java
+++ chapter-1-building-blocks/src/main/java/HelloWorld.java
@@ -1,7 +1,35 @@
 public class HelloWorld {
 
+    public static void test1() {
+        final int score1 = 8, score2 = 3;
+
+        char myScore = 7;
+
+        var goal = switch (myScore) {
+            default -> { yield "unknown"; }
+            case  score1 -> "great";
+            case 2, 4, 6 -> "good";
+            case score2, 0 -> {yield "bad";} // yield required, was missing
+        };
+
+        System.out.println( goal);
+
+    };
+
+    public static void test2() {
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");    
+
+        double jupiter = ( 3 + 9 ) - 1.0f;
+
+        var x = 2 + 2 * 3;
+
+        System.out.println("Hello World" + x);    
+
+
+        test1();
     }
 
 }
